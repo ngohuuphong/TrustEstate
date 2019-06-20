eb3030f3f61676baddba0416db8de11b19e558a3
ngo_huu_phong <ngo_huu_phong@management-partners.co.jp>
2019-06-20 17:38:41 +0700

    update

--- src/Tabs/LoginTab/login.js
+++ src/Tabs/LoginTab/login.js
@@ -1,29 +1,91 @@
-import React, { Component } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome5';
-import Styles from '../../../styles';
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  SafeAreaView,
+  Dimensions
+} from "react-native";
+import {
+  Container,
+  Header,
+  Content,
+  Item,
+  Input,
+  Icon,
+  Button
+} from "native-base";
+import Fontawesome from "react-native-vector-icons/FontAwesome5";
+
+import Styles from "../../../styles";
+const { width, height } = Dimensions.get("window");
 
 class LoginScreen extends Component {
-
-    static navigationOptions = ({ navigation }) => ({
-        title: "Profile",
-        headerLeft: (
-            <TouchableOpacity
-                style={Styles.headerButton}
-                onPress={() => navigation.openDrawer()}>
-                <Icon name="bars" size={20} />
-            </TouchableOpacity>
-        ),
-
-    })
-    render() {
-        return (
-            <View style={Styles.container}>
-                <Text>Profile Screen</Text>
-                <Icon name="user-circle" size={48} />
-            </View>
-        );
-    }
+  static navigationOptions = ({ navigation }) => ({
+    title: "Login",
+    headerLeft: (
+      <TouchableOpacity
+        style={Styles.headerButton}
+        onPress={() => navigation.openDrawer()}
+      >
+        <Fontawesome name="bars" size={20} />
+      </TouchableOpacity>
+    )
+  });
+  render() {
+    return (
+      <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            paddingHorizontal: 40,
+            color: "green"
+          }}
+        >
+          <Item rounded style={{ borderRadius: 5 }}>
+            <Icon style={{ color: "#b4b4b4" }} active name="mail" />
+            <Input style={{ color: "#b4b4b4" }} placeholder="Email" />
+          </Item>
+          <Item rounded style={{ borderRadius: 5,marginTop: 10 }}>
+            <Icon style={{ color: "#b4b4b4" }} active name="key" />
+            <Input
+              style={{ color: "#b4b4b4"}}
+              secureTextEntry={true}
+              placeholder="Password"
+            />
+          </Item>
+          <View style={{}}>
+                <View></View>
+                <View></View>
+          </View>
+          <Button
+            style={{
+              width: width - 80,
+              marginTop: 30,
+              justifyContent: "center",
+              borderRadius: 5
+            }}
+            success
+          >
+            <Text
+              style={{
+                color: "white",
+                textTransform: "uppercase",
+                fontWeight: "400",
+                fontSize: 15
+              }}
+            >
+              {" "}
+              Login{" "}
+            </Text>
+          </Button>
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 
 export default LoginScreen;
